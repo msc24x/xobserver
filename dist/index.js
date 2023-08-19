5cba4f6841a32626a19baab3acfdceb4cc6f450a
Maninderpal Singh <msc24x@gmail.com>
2023-08-19 14:28:17 +0530

    chore: gitignore

--- dist/index.js
+++ dist/index.js
@@ -16,13 +16,19 @@ export class XObserver {
         }
         return null;
     }
+    /**
+     * Application must call this method to ensure the existence of a particular scope
+     * @param scope Unique Identifier for a particular use case of IntersectionObserver
+     * @param options They are the {@link IntersectionObserverInit} options
+     * @returns void
+     */
     static ping(scope, options = {
         rootMargin: "0px",
         threshold: 0,
     }) {
         const observer = this.getEntry(scope);
         if (observer) {
-            return observer.observer;
+            return;
         }
         const newObserver = new IntersectionObserver((entries) => {
             for (let entry of entries) {
@@ -34,8 +40,15 @@ export class XObserver {
             observer: newObserver,
             subscribers: [],
         });
-        return newObserver;
+        return;
     }
+    /**
+     * Use this static method to subscribe an element to a particular scope.
+     * @param scope Unique Identifier for a particular use case of IntersectionObserver
+     * @param element The element to observe
+     * @param callback Callback ({@link XObserverCallback}) when the element intersects
+     * @returns void
+     */
     static subscribe(scope, element, callback) {
         const observer = this.getEntry(scope);
         if (!observer)
@@ -54,6 +67,15 @@ export class XObserver {
         });
         return;
     }
+    /**
+     * Use this when an element is no longer required to be observed.
+     *
+     * Note: it is recommended to use this method since the XObserver will not destroy the underlying
+     * IntersectionObserver instance if the elements are still connected to it.
+     * @param scope Unique Identifier for a particular use case of IntersectionObserver
+     * @param element The element to disconnect from the IntersectionObserver instance
+     * @returns void
+     */
     static unsubscribe(scope, element) {
         const observerEntry = this.getEntry(scope);
         if (!observerEntry)
